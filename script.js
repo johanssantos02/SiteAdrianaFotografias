2e43658dfdaf975c0031e6f5534b58bc49091b15
Johan Silva Cardoso <https://github.com/johanssantos02/AdrianaFotografias.git>
2025-05-26 23:09:18 -0300

    Comitando os portifolios

---
+++ script.js
@@ -0,0 +1,52 @@
+$(function () {
+  $('.imagens').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    asNavFor: '.imagens',
+    infinite: true,
+  });
+  $('.img-nav').slick({
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    asNavFor: '.imagens',
+    centerMode: true,
+    centerPadding: '100px',
+    focusOnSelect: true,
+    adaptiveHeight: true,
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    infinite: true,
+    variableWidth: true,
+    
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 1
+        }
+      }
+    ]
+
+  });
+});
+
+// var botao = document.querySelector('input[type=button]');
+
+// botao.addEventListener('click', () => {
+//   alert('Mensagem enviada com sucesso!')
+// });
+
+
